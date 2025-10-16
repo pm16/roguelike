ae2c768ae687637fd24ccfc2778f2d81954eb881
Comrade Puffin <comradepuffin@pm.me>
2025-10-16 14:30:37 +0700

    Most of the way through part 1.

--- test-tcod.py
+++ test-tcod.py
@@ -12,6 +12,19 @@ import tcod as libtcod
 ASSETS_FOLDER = "assets"
 FONT_FILE = os.path.join(ASSETS_FOLDER, "Alloy_curses_12x12.png")
 
+class ExampleState:
+    player_x : int
+    player_y : int
+    def __init__(self, x, y) :
+        self.player_x = x
+        self.player_y = y
+
+    def on_draw(self, console : libtcod.console.Console) -> None :
+        console.print(self.player_x, self.player_y, "@")
+
+
+
+
 def main() -> None :
     screen_width = 80
     screen_height = 50
@@ -19,10 +32,13 @@ def main() -> None :
     tileset = libtcod.tileset.load_tilesheet(FONT_FILE, columns = 16, rows = 16, charmap = libtcod.tileset.CHARMAP_CP437)
     libtcod.tileset.procedural_block_elements(tileset = tileset)
     console = libtcod.console.Console(screen_width, screen_height)
-    console.print(0, 0, "Hello World")
+    state = ExampleState(x = console.width // 2, y = console.height // 2)
+
 
     with libtcod.context.new(tileset = tileset) as context :
         while True :
+            console.clear()
+            state.on_draw(console)
             context.present(console)
             for event in libtcod.event.wait() :
                 print(event)
