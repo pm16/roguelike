e66a1e433e45e4e31eac091aeb8f6c5b6523b8e7
Comrade Puffin <comradepuffin@pm.me>
2025-10-21 20:34:57 +0700

    Added a line drawing box around the menu.

--- Menu.py
+++ Menu.py
@@ -1,4 +1,14 @@
 import tcod as libtcod
+from enum import StrEnum
+
+class BOX(StrEnum) :
+    V_LINE = "│",
+    H_LINE = "─",
+    UL_CORNER = "┌",
+    UR_CORNER = "┐",
+    LL_CORNER = "└",
+    LR_CORNER =  "┘"
+
 
 class Menu :
     items = [str]
@@ -7,6 +17,7 @@ class Menu :
     cursor_pos : int
     x_pos : int
     y_pos : int
+
 
     def __init__(self, title : str, cursor : str, x : int, y : int) :
         self.title = title
@@ -26,6 +37,26 @@ class Menu :
         return self.items[self.cursor_pos]
     
     def draw(self, console : libtcod.console.Console) -> None :
+        longest = 0
+        for x in range(len(self.items)) :
+            if len(self.items[x]) > len(self.items[(x + 1) % len(self.items)]) :
+                longest = len(self.items[x])
+        if len(self.title) > longest :
+            longest = len(self.title)
+        for i in range(longest + 2) :
+            console.print(self.x_pos + i -1, self.y_pos  - 1, BOX.H_LINE)
+            console.print(self.x_pos + i -1, self.y_pos + len(self.items) +1, BOX.H_LINE)
+
+        for i in range(len(self.items) + 1) :
+            console.print(self.x_pos - 2, self.y_pos + i, BOX.V_LINE)
+            console.print(self.x_pos + longest + 1, self.y_pos + i, BOX.V_LINE)
+
+        console.print(self.x_pos -2, self.y_pos - 1, BOX.UL_CORNER)
+        console.print(self.x_pos + longest + 1, self.y_pos - 1, BOX.UR_CORNER)
+        console.print(self.x_pos -2, self.y_pos + len(self.items) + 1, BOX.LL_CORNER)
+        console.print(self.x_pos + longest + 1, self.y_pos + len(self.items) + 1, BOX.LR_CORNER)
+
+
         console.print(self.x_pos, self.y_pos, self.title)
         y = 1
         for item in self.items :
